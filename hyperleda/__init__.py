b99529179e269dc8979725ade537abe132f1cc09
Artyom Zaporozhets <kraysent@gmail.com>
2024-08-16 21:55:38 +0300

    updated model and ruff excluded errors list

--- hyperleda/__init__.py
+++ hyperleda/__init__.py
@@ -2,3 +2,8 @@ from hyperleda.client import HyperLedaClient
 from hyperleda.config import *
 from hyperleda.error import APIError
 from hyperleda.model import *
+
+__all__ = [
+    "HyperLedaClient",
+    "APIError",
+]
